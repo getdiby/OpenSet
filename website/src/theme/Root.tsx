e6641f9002ce9959f951e8db5cdb792abe0099c2
Stipe <sbuljat@gmail.com>
2026-03-06 07:38:13 +0100

    Remove PasswordGate component from Root.tsx to simplify the rendering of children.

--- website/src/theme/Root.tsx
+++ website/src/theme/Root.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import PasswordGate from '@site/src/components/PasswordGate';
 
 export default function Root({ children }: { children: React.ReactNode }) {
-  return <PasswordGate>{children}</PasswordGate>;
+  return <>{children}</>;
 }
